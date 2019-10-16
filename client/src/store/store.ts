de72d9bfe07c8c07c410654eccc2de954cc399b5
Jakub Kanitra <>
2019-10-16 17:45:59 +0200

    Ignore additional window global

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,8 +2,8 @@ import { combineReducers, createStore } from 'redux';
 import { reducers } from './reducers';
 import { UsersState } from './reducers/UsersReducer';
 
-// @ts-ignore
 const REDUX_DEV_TOOLS_ENHANCER =
+  // @ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export interface State {
