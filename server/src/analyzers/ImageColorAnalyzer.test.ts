a3e3a5656e30ced04508325a8773d5dc25a2ecfa
Peter Berezny <spontain@gmail.com>
2019-10-28 10:37:27 +0100

    This is a working example that adds node-imagemagick as a dependency. - I am not sure this should go in longterm since that dependency is super old and unmaintained

--- server/src/analyzers/ImageColorAnalyzer.test.ts
+++ server/src/analyzers/ImageColorAnalyzer.test.ts
@@ -1,12 +1,19 @@
+import * as Debugger from 'debug';
 import * as Path from 'path';
 import { getImageFeatures } from './ImageColorAnalyzer';
 
-const imageFilePath: string = Path.resolve(__dirname, '__fixtures/RedImage.png');
+const debug = Debugger('sup:test:ImageColorAnalyzer');
+const imageFilePath: string = Path.resolve(
+  __dirname, '__fixtures__/RedImage.png'
+);
 
 describe(`Image color analyzer [ICA]`, () => {
   it(`Should retrieve image features`, async () => {
     const features = await getImageFeatures(imageFilePath);
 
-    expect(features).toBe(null);
+    debug('features', features);
+    expect(features.format).toBe('PNG');
+    expect(features.width).toBe(400);
+    expect(features.height).toBe(400);
   })
 })
--- server/src/analyzers/ImageColorAnalyzer.ts
+++ server/src/analyzers/ImageColorAnalyzer.ts
@@ -1,10 +1,12 @@
+import * as Debugger from 'debug';
 import * as ImageMagick from 'imagemagick';
 import { promisify } from 'util';
 
+const debug = Debugger('sub:ImageColorAnalyzer');
 type LibCallback = (error: Error, features: ImageMagick.Features) => void;
 
 export function getImageFeatures(filePath: string): Promise<ImageMagick.Features> {
-  console.log('filePath', filePath);
+  debug('filePath', filePath);
   return promisify(
     (done: LibCallback) => ImageMagick.identify(filePath, done)
   )();
