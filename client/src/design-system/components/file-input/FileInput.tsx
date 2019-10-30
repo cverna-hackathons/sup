800118af13c933b55c879e5c99033f0808987ee5
Jakub Kanitra <jakub.kanitra@nextbyteapps.com>
2019-10-30 19:12:10 +0100

    feat: Create default button style

--- client/src/design-system/components/file-input/FileInput.tsx
+++ client/src/design-system/components/file-input/FileInput.tsx
@@ -2,69 +2,83 @@ import React from 'react';
 import './FileInput.scss';
 
 interface Props {
-    primaryText: string;
-    secondaryText: string;
-    inputText: string;
-    confirmBtnText: string;
-    onSelect(file: File): void;
+  primaryText: string;
+  secondaryText: string;
+  inputText: string;
+  confirmBtnText: string;
+  onSelect(file: File): void;
 }
 
 interface State {
-    file?: File;
+  file?: File;
 }
 
 export class FileInput extends React.PureComponent<Props, State> {
-    state: State = {};
+  state: State = {};
 
-    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-            this.setState({
-                file: event.target.files[0]
-            });
-        }
-    };
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      this.setState({
+        file: event.target.files[0],
+      });
+    }
+  };
 
-    handleDrag = (event: React.DragEvent<HTMLElement>) => {
-      if (event.dataTransfer.files && event.dataTransfer.files[0]) {
-          this.setState({
-              file: event.dataTransfer.files[0],
-          });
-      }
-      event.preventDefault();
-    };
+  handleDrag = (event: React.DragEvent<HTMLElement>) => {
+    if (event.dataTransfer.files && event.dataTransfer.files[0]) {
+      this.setState({
+        file: event.dataTransfer.files[0],
+      });
+    }
+    event.preventDefault();
+  };
 
-    handleDragOver = (event: React.DragEvent<HTMLElement>) => {
-        event.preventDefault();
-    };
+  handleDragOver = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+  };
 
-    handleConfirmation = () => {
-        if (!this.state.file) {
-            return;
-        }
-        this.props.onSelect(this.state.file);
-    };
+  handleConfirmation = () => {
+    if (!this.state.file) {
+      return;
+    }
+    this.props.onSelect(this.state.file);
+    this.setState({
+      file: undefined,
+    });
+  };
 
-    renderConfirmation() {
-        if (!this.state.file) {
-            return null;
-        }
-
-        return (
-            <button onClick={this.handleConfirmation}>{this.props.confirmBtnText} {this.state.file.name}</button>
-        );
+  renderConfirmation() {
+    if (!this.state.file) {
+      return null;
     }
 
-    render() {
-        return (
-            <div className="file-input-wrapper" onDrop={this.handleDrag} onDragOver={this.handleDragOver}>
-                <span>
-                    <strong>{this.props.primaryText}</strong>
-                    <label htmlFor="fileInput">{this.props.inputText}</label>
-                </span>
-                <p>{this.props.secondaryText}</p>
-                {this.renderConfirmation()}
-                <input id="fileInput" accept="image/*" type="file" onChange={this.handleChange} />
-            </div>
-        );
-    }
+    return (
+      <button onClick={this.handleConfirmation}>
+        {this.props.confirmBtnText} {this.state.file.name}
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div
+        className="file-input-wrapper"
+        onDrop={this.handleDrag}
+        onDragOver={this.handleDragOver}
+      >
+        <span>
+          <strong>{this.props.primaryText}</strong>
+          <label htmlFor="fileInput">{this.props.inputText}</label>
+        </span>
+        <p>{this.props.secondaryText}</p>
+        {this.renderConfirmation()}
+        <input
+          id="fileInput"
+          accept="image/*"
+          type="file"
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
