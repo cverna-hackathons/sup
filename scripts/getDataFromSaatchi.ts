33789cee07a61e72766b6ded7476c44bf07e27b9
Peter Berezny <spontain@gmail.com>
2020-01-02 21:33:22 +0100

    calling the page

--- scripts/getDataFromSaatchi.ts
+++ scripts/getDataFromSaatchi.ts
@@ -3,16 +3,34 @@ import { promisify } from 'util';
 
 const Wait = promisify(setTimeout);
 
-
-main().finally(() => {
-  process.stdout.write(`Exiting \n`);
-  process.exit();
-});
-
 async function main(): Promise<void> {
-  process.stdout.write(`Running \n`);
+  console.info(`Running \n***`);
+
+  const browser: Puppeteer = await Puppeteer.launch();
 
   let nextId: number = 1;
+  let shouldContinue = true;
 
+  while(shouldContinue) {
+    const url: string = `https://www.saatchiart.com/account/artworks/${nextId}`
+    const data = await getDataFromUrl(url, browser);
+
+    console.info(`[${nextId.toString().padStart(9, ' ')}] data from: ${url}`);
+    
+    await Wait(Math.random() * 5000);
+    nextId++;
+  }
   return;
 }
+
+async function getDataFromUrl(url: string, browser: Puppeteer.Browser) {
+  const page: Puppeteer.page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+  await page.goto(url);
+}
+
+main().finally(() => {
+  console.info(`Exiting.`);
+  process.exit();
+});
