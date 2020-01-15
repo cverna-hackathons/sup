f2fe77042b1e86fc46597ce198774cf8d3a642a6
Peter Berezny <spontain@gmail.com>
2020-01-15 22:10:04 +0100

    working ID extraction

--- probes/saatchi/main.go
+++ probes/saatchi/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 import "net/http"
 import "bytes"
+import "regexp"
 
 func main() {
+    var hostIdRegexp = regexp.MustCompile(`applicationId\"\:\"(?P<id>.*)\"\,\"nextApp`)
+
     resp, _ := http.Get("https://www.saatchiart.com/paintings/fine-art")
     buf := new(bytes.Buffer)
     buf.ReadFrom(resp.Body)
-	fmt.Println(buf.String())
+
+    var responseString = buf.String()
+    var hostId = hostIdRegexp.FindStringSubmatch(responseString)
+
+    fmt.Println(hostId[1])
+	// fmt.Println(responseString)
 }
