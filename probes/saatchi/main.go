354c6c9a2143ec1ef0fea80c561d28dce937dbf0
Peter Berezny <spontain@gmail.com>
2020-01-16 16:50:58 +0100

    wip

--- probes/saatchi/main.go
+++ probes/saatchi/main.go
@@ -4,6 +4,7 @@ import "fmt"
 import "net/http"
 import "bytes"
 import "regexp"
+import "strings"
 
 func main() {
     var hostIdRegexp = regexp.MustCompile(`applicationId\"\:\"(?P<id>.*)\"\,\"nextApp`)
@@ -13,8 +14,20 @@ func main() {
     buf.ReadFrom(resp.Body)
 
     var responseString = buf.String()
-    var hostId = hostIdRegexp.FindStringSubmatch(responseString)
+    var hostId = hostIdRegexp.FindStringSubmatch(responseString)[1]
+    
+    // posting to: https://b0ml7g848r-dsn.algolia.net/1/indexes/production_all_artworks/query
+    var hostUrl = (
+        "https://" + strings.ToLower(hostId) + 
+        "-dsn.algolia.net/1/indexes/production_all_artworks/query" +
+        "?x-algolia-application-id=" + hostId)
+    
+    contentResponse, _ := http.Post(hostUrl, "application/x-www-form-urlencoded")
+    contentBuffer := new(bytes.Buffer)
+    contentBuffer.ReadFrom(contentResponse.Body)
 
-    fmt.Println(hostId[1])
+    responseString := contentBuffer.String()
+    fmt.Println(hostUrl)
+    fmt.Println(responseString)
 	// fmt.Println(responseString)
 }
